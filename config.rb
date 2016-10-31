c2d44483a574192701f5b914a8e2c1eb68777721
nicholassiskos <nicholasasiskos@gmail.com>
2016-10-31 22:36:23 +0000

    Automated commit at 2016-10-31 22:36:23 UTC by middleman-deploy 1.0.0

--- config.rb
+++ config.rb
@@ -31,6 +31,10 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
+
 ###
 # Helpers
 ###
@@ -73,9 +77,6 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-  #
-  require_relative "./lib/build_cleaner"
-  activate :build_cleaner
 end
 
 # Deployment
